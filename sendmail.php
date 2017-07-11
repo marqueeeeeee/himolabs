33069b688a6eb840a0c230bb4b56960d7f481d09
mark.sancho <mark.sancho@NDPHPBI7-MSANCH>
2017-07-11 14:06:46 +0800

    updates

--- sendmail.php
+++ sendmail.php
@@ -10,7 +10,7 @@ Message: ".$txt."
 $to = "himolabs@gmail.com";
 $subject = "HimoLabs Contact Us";
 $headers = "From: " . "\r\n" .
-"CC: glenn.espejo0112@gmail.com, jeealisongloria@gmail.com, markivankarlsancho@gmail.com, freyja@freedom@gmail.com";
+"CC: glenn.espejo0112@gmail.com, jeealisongloria@gmail.com, markivankarlsancho@gmail.com, freyja@freedom@gmail.com, nathaniel.zamora.caceres@gmail.com";
 mail($to,$subject,$message,$headers);
 
 header( 'Location: http://www.himolabs.com/' ) ;
